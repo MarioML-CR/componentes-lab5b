0d2adb99a2b68047f4d9f568517d01fce0560c87
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2021-07-14 08:48:47 -0600

    Se agrega el método getJournal al JournalController

--- src/main/java/com/cenfotec/boot/controllers/JournalController.java
+++ src/main/java/com/cenfotec/boot/controllers/JournalController.java
@@ -1,10 +1,15 @@
 package com.cenfotec.boot.controllers;
 
+import com.cenfotec.boot.domian.Journal;
 import com.cenfotec.boot.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class JournalController {
@@ -16,5 +21,10 @@ public class JournalController {
     public String index(Model model) {
         model.addAttribute("journal", repo.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = "/journal", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public @ResponseBody List<Journal> getJournal () {
+        return repo.findAll();
     }
 }
